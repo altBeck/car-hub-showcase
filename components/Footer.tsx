560edea0bcb6ee147179b632fc5c94a46c75f21b
Kanno Beck <drkannobeck@gmail.com>
2023-06-09 15:19:11 +0100

    update-footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,65 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { footerLinks } from "@/constants/contants";
+import { link } from "fs";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-center gap-6">
+          <Image 
+            src="/logo.svg"
+            alt="logo"
+            width={118}
+            height={18}
+            className="object-obtain"
+          />
+          <p className="text-base text-gray-700">
+            Tellso CarHub 2023 
+            <br />
+            All rights reserved &copy;
+          </p>
+        </div>
+
+        <div className="footer__links">
+          {footerLinks.map((link) => (
+            <div key={link.title} className="footer__link">
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((items) => (
+                <Link
+                  key={items.title}
+                  href={items.url}
+                  className="text-gray"
+                >
+                  {items.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+
+        <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+          <p>© 2023 All Rights Reserved, Tellso CarHub</p>
+          <div 
+            className="footer__copyrights-link">
+            <Link 
+              href="/"
+              className="text-gray-500"
+            >
+              Privacy policy
+            </Link>
+            <Link 
+              href="/"
+              className="text-gray-500"
+            >
+              Terms of use
+            </Link>
+          </div>
+        </div>
+    </footer>
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,8 @@ const Navbar = () => {
   return (
     <header className="w-full absolute z-10">
       <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
-        <link href="/" className="flex justify-center items-center">
+
+        <Link href="/" className="flex justify-center items-center">
           <Image
             src="/logo.svg"
             alt="Tellso Car Hub Logo"
@@ -15,7 +16,13 @@ const Navbar = () => {
             height={18}
             className="object-contain"
           />
-        </link>
+        </Link>
+
+        <CustomButton 
+          title="Sign in"
+          btnType="button"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+        />
       </nav>
     </header>
   )
